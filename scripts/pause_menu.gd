36670ba751a7091bac4be90af260c1a1ed63a535
Turtike <turtike@gmail.com>
2024-09-13 14:48:53 +0800

    fix: water shader scale, other stuff

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,14 +1,28 @@
 extends Control
 
 @export var scroll_background: TextureRect
-@export var master_volume_slider: HSlider
+@export var master_volume_slider: Range
+@export var brightness_slider: Range
 @export var volume_min: float = -50.0
 @export var volume_max: float = 50.0
+@export var brightness_min: float = 0
+@export var brightness_max: float = 0.1
+
+var player_camera: Camera3D
 
 var scroll_speed: float = 5
 
 func _ready() -> void:
-	master_volume_slider.ratio = inverse_lerp(volume_min, volume_max, master_volume_slider.ratio)
+	var index = AudioServer.get_bus_index("Master")
+	master_volume_slider.ratio = inverse_lerp(volume_min, volume_max, AudioServer.get_bus_volume_db(index))
+	
+	var cameras = get_tree().get_nodes_in_group("player_camera")
+	if !cameras:
+		return
+	player_camera = (cameras[0] as Camera3D)
+	if player_camera == null:
+		return
+	brightness_slider.ratio = inverse_lerp(brightness_min, brightness_max, player_camera.environment.background_energy_multiplier)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel") and !$AnimationPlayer.is_playing():
@@ -42,3 +56,9 @@ func _on_master_volume_changed(value):
 		return
 	AudioServer.set_bus_mute(index, false)
 	AudioServer.set_bus_volume_db(index, lerpf(volume_min, volume_max, master_volume_slider.ratio));
+
+func _on_brightness_changed(value):
+	if player_camera == null:
+		return
+	player_camera.environment.background_energy_multiplier = lerpf(brightness_min, brightness_max, brightness_slider.ratio)
+	#player_camera.environment.tonemap_exposure = value
